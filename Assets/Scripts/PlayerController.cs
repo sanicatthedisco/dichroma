c888296468d35a5f76e2aea894f500d56bf64b65
sanicatthedisco <jaredlenn90@gmail.com>
2019-05-25 15:07:23 -0400

    tiles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    float dx, ddx, speed;
+    float friction;
     private Rigidbody2D rb;
 
 
@@ -12,14 +13,17 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        friction = 5;
+        speed = 10;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveHor = Input.GetAxis("Horizontal");
+        dx = Input.GetAxis("Horizontal");
+        ddx += dx;
 
-        Vector2 movement = new Vector2(moveHor, rb.velocity.y);
+        Vector2 movement = new Vector2(dx, rb.velocity.y);
 
         rb.velocity = movement;
     }
