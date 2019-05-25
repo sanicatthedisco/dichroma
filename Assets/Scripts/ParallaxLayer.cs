9087de4880a603d6ddc66da090970e9addf14a6b
munselljbx <33090112+munselljbx@users.noreply.github.com>
2019-05-25 16:12:06 -0400

    Parallax
    Disabled for now

--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -3,39 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class ParallaxBackground : MonoBehaviour
+public class ParallaxLayer : MonoBehaviour
 {
-    public ParallaxCamera parallaxCamera;
-    List<ParallaxLayer> parallaxLayers = new List<ParallaxLayer>();
-
-    void Start()
+    public float parallaxFactor;
+    public void Move(float delta)
     {
-        if (parallaxCamera == null)
-            parallaxCamera = Camera.main.GetComponent<ParallaxCamera>();
-        if (parallaxCamera != null)
-            parallaxCamera.onCameraTranslate += Move;
-        SetLayers();
-    }
-
-    void SetLayers()
-    {
-        parallaxLayers.Clear();
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            ParallaxLayer layer = transform.GetChild(i).GetComponent<ParallaxLayer>();
-
-            if (layer != null)
-            {
-                layer.name = "Layer-" + i;
-                parallaxLayers.Add(layer);
-            }
-        }
-    }
-    void Move(float delta)
-    {
-        foreach (ParallaxLayer layer in parallaxLayers)
-        {
-            layer.Move(delta);
-        }
+        Vector3 newPos = transform.localPosition;
+        newPos.x -= delta * parallaxFactor;
+        transform.localPosition = newPos;
     }
 }
+
